d4c7f2a0ba1fc6cbdf6300f6ccb11a7dbfcc1db0
RashedAbdullah <maarashed40@gmail.com>
2025-03-09 17:38:06 +0600

    Nex.tjs

--- app/page.js
+++ app/page.js
@@ -13,31 +13,22 @@ import MuamalahSection from "@/components/muamalah-section";
 const HomePage = () => {
   return (
     <div className="container py-5 space-y-5">
-      {/* ক্যালেন্ডার সেকশন */}
       <CalendarSection />
 
-      {/* আইকন বক্স */}
       <DailyWorks />
 
-      {/* নামায ট্র্যাকার */}
       <SalahSection />
 
-      {/* সিয়াম সেকশন */}
       <SiyamSection />
 
-      {/* অজিফা সেকশন */}
       <OjifaSection />
 
-      {/* ইলম সেকশন */}
       <IlmSection />
 
-      {/* দাওয়াহ সেকশন */}
       <DawahSection />
 
-      {/* মুআমালাহ সেকশন */}
       <MuamalahSection />
 
-      {/* সাদাকাহ সেকশন */}
       <SadaqahSection />
     </div>
   );
