b56900fc93a7d3153bd9971bbab63d750c3c3ded
RashedAbdullah <maarashed40@gmail.com>
2025-03-10 15:52:49 +0600

    Database style has been created

---
+++ data/data-sync.js
@@ -0,0 +1,172 @@
+export const muhasabahbyDate = {
+  userId: "",
+  data: [
+    {
+      date: "",
+      trackers: [
+        {
+          title: "নামাজ ্ ট্রেকার",
+          prayars: [
+            {
+              name: "ফজর",
+              value: true,
+            },
+            {
+              name: "যোহর",
+              value: true,
+            },
+            {
+              name: "আছর",
+              value: true,
+            },
+            {
+              name: "মাগরিব",
+              value: true,
+            },
+            {
+              name: "ইশা",
+              value: true,
+            },
+          ],
+        },
+        {
+          title: "সিয়াম ট্রেকার",
+          prayars: [
+            {
+              name: "আজকে রোযা রেখেছেন?",
+              value: true,
+            },
+          ],
+        },
+        {
+          title: "কুরআন সেকশন",
+          prayars: [
+            {
+              name: "আজকে কুরআন তিলাওয়াত করেছেন?",
+              value: true,
+            },
+            {
+              name: "আয়াত সংখ্যা?",
+              value: 343,
+            },
+          ],
+        },
+        {
+          title: "সকাল-সন্ধ্যার আমল",
+          prayars: [
+            {
+              name: "ফজরের আমল",
+              value: true,
+            },
+            {
+              name: "মাগরিরের আমল",
+              value: false,
+            },
+          ],
+        },
+        {
+          title: "জুম’আর আমল",
+          prayars: [
+            {
+              name: "৮০ বার দরুদ শরীফ",
+              value: true,
+            },
+            {
+              name: "সুরা কাহফ",
+              value: false,
+            },
+          ],
+        },
+        {
+          title: "ঘুমের আমল",
+          prayars: [
+            {
+              name: "সুরা মুলক",
+              value: true,
+            },
+            {
+              name: "সুরা ইখলাস, ফালাক, নাস",
+              value: false,
+            },
+            {
+              name: "আয়াতুল কুরসী",
+              value: false,
+            },
+            {
+              name: "সূরা বাকারার শেষ দুই আয়াত",
+              value: false,
+            },
+          ],
+        },
+        {
+          title: "ইলম সেকশন",
+          prayars: [
+            {
+              name: "আজকে ইলম শিখেছেন?",
+              value: true,
+            },
+            {
+              name: "ইলম নোট",
+              value: "",
+            },
+            {
+              name: "সাপ্তাহিক ইলমী মজলিশে বসেছেন?",
+              value: false,
+            },
+            {
+              name: "মজলিশ নোট",
+              value: "",
+            },
+          ],
+        },
+        {
+          title: "দাওয়াহ সেকশন",
+          prayars: [
+            {
+              name: "আজকে দাওয়াত দিয়েছেন?",
+              value: true,
+            },
+            {
+              name: "কতজনকে দিয়েছেন?",
+              value: 5,
+            },
+            {
+              name: "দাওয়াহ নোট",
+              value: "",
+            },
+          ],
+        },
+        {
+          title: "মুআমালাহ-মুআশারাত সেকশন",
+          prayars: [
+            {
+              name: "আজকে মিথ্যা কথা বলেছেন?",
+              value: true,
+            },
+            {
+              name: "আজকে কারো গীবত করেছেন?",
+              value: true,
+            },
+            {
+              name: "আজকে কারো সাথে প্রতারণা করেছেন?",
+              value: false,
+            },
+          ],
+        },
+        {
+          title: "দৈনিক সাদাকাহ",
+          prayars: [
+            {
+              name: "আজকে সাদাকাহ আদায় করেছেন?",
+              value: true,
+            },
+            {
+              name: "সাদাকাহর পরিমাণ",
+              value: true,
+            },
+          ],
+        },
+      ],
+    },
+  ],
+};
